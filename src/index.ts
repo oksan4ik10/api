3fe535b69ef1c6213e0faefcd50c5c05bee18a16
Oksana Kalinina <ksan4ik92@gmail.com>
2023-01-13 11:03:40 +0300

    feat: add html+css

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-function d(a: number) {
-  return a;
-}
+const t = "http://127.0.0.1:3000";
+const test = async (local: string) => {
+  const url = `${local}\\garage`;
+  const response = await fetch(url);
 
-console.log(d(4));
+  const commits = await response.json(); // читаем ответ в формате JSON
+
+  console.log(commits);
+};
+
+test(t);
